9d849bcdc2440f3be2c3d0ae59e395656c5b4a5a
Martin Matovu <matovu.synergy@gmail.com>
2019-09-06 14:51:51 +0300

    Import UserList, pass it as an expression to `list` prop of <Resource/>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Admin, Resource, ListGuesser } from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server'
+import { Admin, Resource } from 'react-admin'
+import { UserList } from './users'
 
 const dataProvider = jsonServerProvider(
   'http://jsonplaceholder.typicode.com'
@@ -8,8 +9,8 @@ const dataProvider = jsonServerProvider(
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name='users' list={ListGuesser} />
+    <Resource name='users' list={UserList} />
   </Admin>
-)
+);
 
 export default App;
